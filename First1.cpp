b328ee2156566c3555068cd3a0c47f9e1ab69f2c
pilliMAHESH <39088870+pilliMAHESH@users.noreply.github.com>
2019-07-24 13:10:24 +0530

    Add files via upload

---
+++ First1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? n;
+
+   raptor_prompt_variable_zzyz ="enter  value";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> n;
+   while (!(n<=10))
+   {
+   }
+   cout << "N value is"+n;
+   return 0;
+}
